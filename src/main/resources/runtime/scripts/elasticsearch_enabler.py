c0b9cda544f5a35d40a0187991ba90359ca6bbd2
fcenedes <fcenedes@tibco.com>
2013-08-29 20:26:05 +0200

    script

---
+++ src/main/resources/runtime/scripts/elasticsearch_enabler.py
@@ -0,0 +1,365 @@
+#
+# Copyright (c) 2013 TIBCO Software Inc. All Rights Reserved.
+# 
+# Use is subject to the terms of the TIBCO license terms accompanying the download of this code. 
+# In most instances, the license terms are contained in a file named license.txt.
+#
+
+from com.datasynapse.fabric.admin.info import AllocationInfo, ComponentInfo, EngineIdInfo, FabricEngineInfo, ComponentAllocationEntryInfo
+from com.datasynapse.fabric.util import GridlibUtils, ContainerUtils
+from com.datasynapse.fabric.common import RuntimeContextVariable, ActivationInfo
+from com.datasynapse.fabric.engine.managedprocess import ManagedProcess
+from com.datasynapse.fabric.container import Feature, Container
+from com.datasynapse.gridserver.admin import Property
+from com.datasynapse.fabric.common import ArchiveActivationInfo
+
+from com.datasynapse.fabric.admin import AdminManager, ComponentAdmin
+from com.datasynapse.fabric.admin.info import GridlibInfo
+
+from jarray import array
+from java.lang import StringBuilder
+import java.lang.System
+from subprocess import Popen, PIPE, STDOUT, call
+
+import os
+import sys, java, types
+import platform
+import time
+import socket
+import fnmatch
+import urllib
+import urllib2, httplib
+import shutil
+import errno
+import shlex
+from java.util import Properties
+
+jarpath = runtimeContext.getVariable('CONTAINER_GRIDLIB_DIR').getValue()
+sys.path.append(os.path.join(jarpath,"ds_jars", "json-path-0.8.1.jar"))
+sys.path.append(os.path.join(jarpath,"ds_jars", "commons-lang-2.6.jar"))
+sys.path.append(os.path.join(jarpath,"ds_jars", "json-smart-1.1.1.jar"))
+from com.jayway.jsonpath import JsonPath as jpath
+
+#######
+# Add Enabler Dependancies
+#######
+
+def getDynamicGridlibDependencies():
+    logInfo("Beginning getDynamicGridlibDependencies()")
+    ELASTIC_VERSION = getElasticSearchVersion()
+    logInfo("ElasticSearch Distribution version is [" + str(ELASTIC_VERSION) + "]")
+    defaultDomainGridlib = GridlibInfo()
+    defaultDomainGridlib.name = "default-domain-type"
+    logInfo("Adding ElasticSearch distribution dependency")
+    gridlib = GridlibInfo()
+    gridlib.name = "elasticsearch-distribution"
+    gridlib.version = str(ELASTIC_VERSION)
+    logInfo("Exiting getDynamicGridlibDependencies()")
+    return array([gridlib, defaultDomainGridlib], GridlibInfo)
+
+
+def getElasticSearchVersion():
+    ElasticSearchVersionVar = proxy.getContainer().getRuntimeContext().getVariable('ElasticSearch_DISTRIBUTION_VERSION')
+    if ElasticSearchVersionVar == None:
+        logInfo("${ElasticSearch_DISTRIBUTION_VERSION} is not set. Defaulting to ElasticSearch Version 0.90.2")
+        ElasticSearchVersion = "0.90.2"
+    else:
+        ElasticSearchVersion = ElasticSearchVersionVar.getValue()   
+    return str(ElasticSearchVersion)
+
+
+def createDir(directory):
+    try:
+        os.makedirs(directory)
+        logInfo(directory + " has been created")
+    except OSError, exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(directory):
+            pass
+        else:
+            raise
+
+def doPlugInsInstall(plugins_distro_zip_path, bindir, plugdir ):
+    plugins_name = os.path.splitext(plugins_distro_zip_path)[0]
+    PLUGINS_ARGS = " -url file://"+ plugins_distro_zip_path + " -install " + plugdir
+    
+    if ContainerUtils.isWindows():
+        PLUGINS_CMD = os.path.join(bindir,"plugin.bat")
+    else:
+        PLUGINS_CMD = os.path.join(bindir,"plugin")
+
+    CMD = PLUGINS_CMD + PLUGINS_ARGS
+    logInfo("Command to be used : " + CMD)
+    return CMD
+            
+###        
+# writes the message in the engine log as INFO level
+###
+def logInfo(msg):
+    logInfo("[ElasticSearch_Enabler] " + msg)
+###        
+# writes the message in the engine log as FINER level
+###      
+def logFiner(msg):
+    logger.finer("[ElasticSearch_Enabler] " + msg)
+###        
+# writes the message in the engine log as SEVERE level
+###        
+def logSevere(msg):
+    logger.severe("[ElasticSearch_Enabler] " + msg)
+
+def getVariableValue(name, value=None):
+    logInfo("get runtime variable value")
+    var = runtimeContext.getVariable(name)
+    if var != None:
+        value = var.value
+    
+    return value
+
+def doInit(additionalVariables):
+    logInfo("Entering doInit() ")
+    ### create a new instance of elasticsearch class
+    elastic = ElasticSearch(additionalVariables)
+    ### store class instance object in an RuntimeContextVariable for use accross all methods
+    elasticRcv = RuntimeContextVariable("ELASTICSEARCH_NODE_OBJECT", elastic, RuntimeContextVariable.OBJECT_TYPE)
+    runtimeContext.addVariable(elasticRcv)
+    logInfo("Exiting doInit() ")
+    
+def doStart():
+    logInfo("Entering doStart() ")
+    elastic = getVariableValue("ELASTICSEARCH_NODE_OBJECT")      
+    if elastic:
+        elastic.startNode()
+    logInfo("Exiting doStart() ")
+
+def doInstall(info):
+    logInfo("Entering  activation info")
+    try:
+       elastic = getVariableValue("ELASTICSEARCH_NODE_OBJECT")
+       if elastic:
+            elastic.installActivationInfo(info)
+    except:
+        type, value, traceback = sys.exc_info()
+        logger.severe("Unexpected error in ELASTICSEARCH:doInstall:" + `value`)
+        
+    logInfo("doInstall:Exit")
+    
+def doUninstall():
+    logInfo("doUninstall()")
+
+def doShutdown():
+    logInfo("Shutting down this node")
+    try:
+        elastic = getVariableValue("ELASTICSEARCH_NODE_OBJECT")
+        elastic.stopNode()
+    except:
+        type, value, traceback = sys.exc_info()
+        logSevere("Unexpected error in ElasticSearch:stopNode:" + `value`)
+    
+def getContainerRunningConditionPollPeriod():
+    return 5000
+
+def isContainerRunning():
+    logInfo("Checking if the Enabler is running")
+    status = None
+    try:
+        elastic = getVariableValue("ELASTICSEARCH_NODE_OBJECT")
+        health = elastic.getNodeStatus()
+        if health == 0:
+            status = True
+        else:
+            status = False
+    except:
+        type, value, traceback = sys.exc_info()
+        logSevere("Unexpected error in ElasticSearch:getNodeStatus:" + `value`)              
+    return status
+####
+# Return Running Conditions Errors
+####
+def getComponentRunningConditionErrorMessage():
+    return "ElasticCache Node failure"
+
+
+
+def getStatistic(statName):
+    stat = None
+    try:
+        elastic = getVariableValue("ELASTICSEARCH_NODE_OBJECT")
+        stat = elastic.getStatistic(statName)
+    except:
+        type, value, traceback = sys.exc_info()
+        logSevere("Unexpected error in ElasticSearch:getStatistic:" + `value`)
+    return stat
+
+class ElasticSearch:
+    
+    def __init__(self, additionalVariables):
+        
+        self.__workdir = getVariableValue('CONTAINER_WORK_DIR')
+        self.__basedir = getVariableValue('ES_BASE_DIR')
+        self.__eshome = runtimeContext.addVariable(RuntimeContextVariable("ES_HOME", self.__basedir, RuntimeContextVariable.ENVIRONMENT_TYPE, "ElasticSearch Home", False, RuntimeContextVariable.NO_INCREMENT))
+        self.__master = runtimeContext.addVariable(RuntimeContextVariable("FIRST_DEPLOYED_MASTER_ADDR", "", RuntimeContextVariable.STRING_TYPE, "Detected Master hostname", False, RuntimeContextVariable.NO_INCREMENT))
+        self.__bindir = os.path.join(self.__basedir , "bin")
+        self.__enginedir = getVariableValue('ENGINE_WORK_DIR')
+     
+        self.__javahome = getVariableValue('GRIDLIB_JAVA_HOME')
+        #runtimeContext.addVariable(RuntimeContextVariable("JAVA_HOME", self.__javahome, RuntimeContextVariable.ENVIRONMENT_TYPE, "JAVA_HOME", False, RuntimeContextVariable.NO_INCREMENT))
+        os.putenv("JAVA_HOME",self.__javahome)
+        
+        self.__UnixPath = self.__bindir + ":" + os.path.join(self.__javahome,"bin") + ":" + os.getenv("PATH")
+        os.putenv("PATH",self.__UnixPath)
+
+        self.__UnixLibPath = os.getenv("LD_LIBRARY_PATH") +":"+ os.path.join(self.__javahome,"jre/lib/amd64/server")+":"+os.path.join(self.__javahome,"jre/lib/amd64/")+":"+os.path.join(self.__javahome,"jre/lib/ext")+":"+os.path.join(self.__javahome,"lib/")
+        os.putenv("LD_LIBRARY_PATH",self.__UnixLibPath)
+        #runtimeContext.addVariable(RuntimeContextVariable("LD_LIBRARY_PATH", self.__UnixLibPath, RuntimeContextVariable.ENVIRONMENT_TYPE, "LD_LIBRARY_PATH", False, RuntimeContextVariable.NO_INCREMENT))
+        
+        self.__UnixPreLoad = os.path.join(self.__javahome,"jre/lib/amd64/libzip.so")
+        os.putenv("LD_PRELOAD",self.__UnixPreLoad )
+        
+        #runtimeContext.addVariable(RuntimeContextVariable("LD_PRELOAD", ldpld, RuntimeContextVariable.ENVIRONMENT_TYPE, "LD_PRELOAD", False, RuntimeContextVariable.NO_INCREMENT))
+        
+        #Setting ES_HOME
+        os.putenv("ES_HOME",self.__basedir)
+        self.__logdir = getVariableValue('ES_LOG_DIR')
+        self.__datadir = getVariableValue('ES_DATA_DIR')
+        self.__tempdir = getVariableValue('ES_TMP_DIR')
+        self.__confdir = getVariableValue('ES_CONF_DIR')
+        self.__plugdir = getVariableValue('ES_PLUGINS_DIR')
+        
+        self.__hostIp = getVariableValue('ES_HOST_IP')
+        self.__httpPort = getVariableValue('HTTP_PORT')
+     
+        self.__pidfile = os.path.join(self.__workdir, "elasticsearch.pid")
+        call(["touch", self.__pidfile])
+        call(["chmod", "777", self.__pidfile])
+        defaultfolders = [self.__logdir, self.__datadir, self.__tempdir , self.__confdir , self.__plugdir, self.__bindir]
+        logInfo("Creating the necessaries folders")
+        for dir in defaultfolders :
+            createDir(dir)
+            call(["chmod", "-fR", "+x", dir])
+            
+        # find the archivesDir
+        #archiveMgmtFeature = ContainerUtils.getFeatureInfo("Archive Management Support", proxy.container, proxy.container.currentDomain)
+        #archivesDir = os.path.join(self.__workdir, archiveMgmtFeature.archiveDirectory)
+        #global archivesDir
+        #logger.severe("Found archives dir " + archivesDir)   
+            
+    def startNode(self):
+        
+        self.__environ = os.environ.copy()
+        
+        ### Starting Node
+        self.__ELASTIC_ARGS = "-p " + self.__pidfile
+        if ContainerUtils.isWindows():
+            self.__ELASTIC_CMD = os.path.join(self.__bindir,"elasticsearch.bat ")
+            self.__CMD = self.__ELASTIC_CMD + self.__ELASTIC_ARGS
+        else:
+            self.__ELASTIC_CMD = os.path.join(self.__bindir,"elasticsearch ")
+            self.__CMD = self.__ELASTIC_CMD + self.__ELASTIC_ARGS
+        logInfo("StartUp Command to be used : " + self.__CMD)
+        args = shlex.split(self.__CMD)
+        process = Popen(args,stdout=None,stderr=None,env=self.__environ,shell=True)
+        process.wait()
+        time.sleep(5)
+        logInfo("Start return Code : " + str(process.returncode))
+        
+    def stopNode(self):
+        self.__endpoint = "/_cluster/nodes/_local/_shutdown"
+        self.__url = "http://"+self.__hostIp+":"+self.__httpPort+self.__endpoint
+        self.__req = urllib2.Request(self.__url, data="")
+        logInfo("shutdown request at : "+ self.__url)
+        try:
+            self.__f = urllib2.urlopen(self.__req)
+        except IOError, e:
+            if hasattr(e, 'reason'):
+                logInfo("Failed to reach the server")
+                logInfo("Reason :" + str(e.reason))
+            elif hasattr(e, 'code'):
+                logInfo("The server couldn\'t fullfill the request.")
+                logInfo("Error code :" + str(e.code))
+        else:
+            dataRaw = self.__f.read()
+            self.__f.close()
+
+    def getStatistic(self, __statname):
+        #split stats path
+        self.__stat = __statname.split(":")          
+        self.__indexname = self.__stat[0]
+        self.__keyname = self.__stat[1]
+        logFiner("Getting Statistics for : "+__statname)    
+        self.__statvalue = 0.0
+        self.__url = "http://"+self.__hostIp+":"+self.__httpPort+"/_nodes/_local/"+self.__indexname+"/stats"
+        logFiner("Retrieving stat from : " + self.__url)
+        self.__req = urllib2.Request(self.__url, None, {'Content-Type': 'application/json'})
+        try:
+            f = urllib2.urlopen(self.__req)
+        except IOError, e:
+            if hasattr(e, 'reason'):
+                logInfo("Failed to reach the server")
+                logInfo("Reason :" + str(e.reason))
+            elif hasattr(e, 'code'):
+                logInfo("The server couldn\'t fullfill the request.")
+                logInfo("Error code :" + str(e.code))
+                logInfo("Error with the URL ?, probably the index is unsupported ??")
+                logInfo("Supported values for statistic index: indices,os,fs,http,jvm,process,thread_pool,transport,network")
+        else:
+            self.__dataRaw = f.read()
+            if len(self.__stat) > 2:
+                self.__subkeyname = self.__stat[2]
+                self.__statvalue = jpath.read(self.__dataRaw, "$.nodes.*."+self.__indexname+"."+self.__keyname+"."+self.__subkeyname)
+            else:
+                self.__statvalue = jpath.read(self.__dataRaw, "$.nodes.*."+self.__indexname+"."+self.__keyname)
+            f.close()
+                     
+        return str(self.__statvalue[0])
+         
+    def doGraceFullRestart(self):
+        self.__url = "http://"+self.__hostIp+":"+self.__httpPort+"/_cluster/nodes/_local/_restart"        
+        self.__response = urllib2.urlopen(self.__url, data="")
+        self.__code = self.__response.code
+        self.__response.close()
+        return self.__code
+     
+    def getNodeStatus(self):
+        self.__returnStatus = 0
+        self.__url = "http://"+self.__hostIp+":"+self.__httpPort+"/_nodes/_local"
+        self.__req = urllib2.Request(self.__url, None, {'Content-Type': 'application/json'})
+        logFiner("Retrieving Status from : "+ self.__url)
+        try:
+            self.__f = urllib2.urlopen(self.__req)
+        except IOError, e:
+            if hasattr(e, 'reason'):
+                logInfo("Failed to reach the server")
+                logInfo("Reason :" + str(e.reason))
+            elif hasattr(e, 'code'):
+                logInfo("The server couldn\'t fullfill the request.")
+                logInfo("Error code :" + str(e.code))
+        else:
+            self.__dataRaw = self.__f.read()
+            self.__status = jpath.read(self.__dataRaw, "$.ok")
+            self.__f.close()
+            logFiner("status : "+ str(self.__status))
+            if (self.__status):
+                logFiner("Node status is OK")
+                self.__returnStatus = 0
+            else :
+                logFiner("Node status is KO")
+                self.__returnStatus = 1
+        
+        return self.__returnStatus
+    
+    def installActivationInfo(self, info):
+        logInfo("install activation info")
+        self.__httpRoutePrefix = getVariableValue("CLUSTER_NAME")
+        try:
+            httpinfo = features.get('HTTP Support')
+            httpinfo.setRouteDirectlyToEndpoints(True)
+            propertyName = "HTTP_STATIC_ROUTE_ElasticSearch_" + self.__httpRoutePrefix
+            prefix = "/elasticsearch/" + self.__httpRoutePrefix +";";
+            propertyValue = prefix + "http://" + self.__hostIp +":" + self.__httpPort
+            info.setProperty(propertyName, propertyValue)
+        except Exception, err:
+            logInfo("Unexpected error: "+ str(sys.exc_info()[0]) +" "+ str(sys.exc_info()[1]))
+
+            
+            
+   
